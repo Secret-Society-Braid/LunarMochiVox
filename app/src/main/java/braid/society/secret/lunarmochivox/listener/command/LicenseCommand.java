ced2b50142598b6582fcfe0941acf79231b856d1
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2026-04-29 02:08:53 +0900

    Refactor LicenseCommand to improve response handling and update license embed formatting

--- app/src/main/java/braid/society/secret/lunarmochivox/listener/command/LicenseCommand.java
+++ app/src/main/java/braid/society/secret/lunarmochivox/listener/command/LicenseCommand.java
@@ -1,9 +1,11 @@
 package braid.society.secret.lunarmochivox.listener.command;
 
+import java.util.concurrent.CompletableFuture;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionContextType;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public class LicenseCommand extends CommandCascade {
 
@@ -15,14 +17,15 @@ public class LicenseCommand extends CommandCascade {
 
   @Override
   protected void respond(SlashCommandInteractionEvent event) {
-    final MessageEmbed embed = new EmbedBuilder()
-      .setTitle("オープンソースライセンス")
-      .addField("オンライン上での確認", "オンライン上での確認は[こちら]()から行えます。", false)
+    CompletableFuture<InteractionHook> deferred = event.deferReply(true).submit();
+    final MessageEmbed licenseEmbed = new EmbedBuilder()
+      .setTitle("オープンソースライセンス",
+        "https://github.com/Secret-Society-Braid/LunarMochiVox/blob/main/docs/clauses/OPEN_SOURCE_LICENSES.md")
+      .addField("オンラインでの確認", "このメッセージタイトルのリンクより確認できます。", false)
       .addField("オフラインでの確認を希望する場合",
-        "個別対応となります。開発者：idol_ranfa_master まで要件を添えてメッセージをお願いします。",
-        false)
+        "個別対応となります。開発者 idol_ranfa_master まで要件を添えてご連絡ください。", false)
+      .setFooter("LunarMochiVox - Secret Society Braid")
       .build();
-    event.deferReply()
-      .queue(success -> success.editOriginalEmbeds(embed).queue());
+    deferred.thenAccept(h -> h.sendMessageEmbeds(licenseEmbed).queue());
   }
 }
