d48528fc11eed030a323bf7f4cf7b58b268c7d1c
iDOL_Ranfa_M@STER <56146205+hizumiaoba@users.noreply.github.com>
2026-04-29 10:55:51 +0900

    Add LicenseCommand for displaying OSS license details (#11)
    * Add LicenseCommand to display OSS license details in both English and Japanese contexts
    
    * Register LicenseCommand in SlashCommandRegistry
    
    * Refactor LicenseCommand to improve response handling and update license embed formatting
    
    * Update LicenseCommand: refine embed content and add spam prevention notice

---
+++ app/src/main/java/braid/society/secret/lunarmochivox/listener/command/LicenseCommand.java
@@ -0,0 +1,33 @@
+package braid.society.secret.lunarmochivox.listener.command;
+
+import java.util.concurrent.CompletableFuture;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+
+public class LicenseCommand extends CommandCascade {
+
+  protected LicenseCommand() {
+    super("license", "Show OSS licenses used in this bot application.");
+    this.addSupportedContext(InteractionContextType.GUILD);
+    this.addSupportedContext(InteractionContextType.BOT_DM);
+  }
+
+  @Override
+  protected void respond(SlashCommandInteractionEvent event) {
+    CompletableFuture<InteractionHook> deferred = event.deferReply(true).submit();
+    final MessageEmbed licenseEmbed = new EmbedBuilder()
+      .setTitle("オープンソースライセンス",
+        "https://github.com/Secret-Society-Braid/LunarMochiVox/blob/main/docs/clauses/OPEN_SOURCE_LICENSES.md")
+      .addField("オンラインでの確認", "このメッセージタイトルのリンクより確認できます。", false)
+      .addField("その他の方法を希望する場合",
+        "個別対応となります。開発者Discord idol_ranfa_master まで要件を添えてご連絡ください。", false)
+      .addField("注意",
+        "メッセージの内容が不適当な場合、スパム対策のため返答いたしませんのでご了承ください。", false)
+      .setFooter("LunarMochiVox - Secret-Society Braid")
+      .build();
+    deferred.thenAccept(h -> h.sendMessageEmbeds(licenseEmbed).queue());
+  }
+}
