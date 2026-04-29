a902a08a723ddeb1b755ad031d22e28c6dc64566
iDOL_Ranfa_M@STER <56146205+hizumiaoba@users.noreply.github.com>
2026-04-29 11:52:08 +0900

    Update workflows to improve Gradle setup and release process (#12)
    * Update release workflow: use `shadowJar` for build and set releases as drafts
    
    * Update workflows: replace manual Gradle setup with `gradle/actions/setup-gradle` and enable caching
    
    * Configure Gradle: add Foojay and Develocity plugins, and enable build scans
    
    * Update workflows: switch Gradle cache provider to `enhanced` for improved caching
    
    * Revert "Update workflows: switch Gradle cache provider to `enhanced` for improved caching"
    
    This reverts commit af14e71158d772b3da6cb73c21b4064e0e58751b.
    
    * Update workflows: re-enable Gradle caching in CI and release workflows

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,21 @@ pluginManagement {
     mavenCentral()
   }
 }
-
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+  id("com.gradle.develocity").version("4.3.2")
+}
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     mavenCentral()
   }
 }
-
+develocity {
+  buildScan {
+    termsOfUseUrl.set("https://gradle.com/terms-of-service")
+    termsOfUseAgree.set("yes")
+  }
+}
 rootProject.name = "LunarMochiVox"
 include("app")
