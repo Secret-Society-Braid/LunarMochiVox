11a8c31f662871b9f4d7b1ed126852669baab002
iDOL_Ranfa_M@STER <hizumiaoba@gmail.com>
2026-05-10 21:10:23 +0900

    Add MetaPropertyUtil to handle meta.properties and its unit test

---
+++ app/src/main/java/braid/society/secret/lunarmochivox/util/MetaPropertyUtil.java
@@ -0,0 +1,34 @@
+package braid.society.secret.lunarmochivox.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
+public class MetaPropertyUtil {
+
+  private MetaPropertyUtil() {}
+
+  public static String getDevUserId() {
+    try {
+      Properties prop = getMetaProperties();
+      String devUserId = Objects.requireNonNullElse(prop, null).getProperty("DEV_USER", null);
+      if (devUserId == null || devUserId.isBlank()) {
+        return null;
+      }
+      return devUserId;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static Properties getMetaProperties() throws IOException {
+    Properties prop = new Properties();
+    InputStream input = MetaPropertyUtil.class.getClassLoader().getResourceAsStream("meta.properties");
+    if (input == null) {
+      return null;
+    }
+    prop.load(input);
+    return prop;
+  }
+}
