aa19ed133314ac3c3718f3173dfc09d3d1dc4c7b
iDOL_Ranfa_M@STER <hizumiaoba@gmail.com>
2026-05-10 13:15:04 +0900

    Add connection check and stub methods for voice channel connect/disconnect handling in VcCommand

--- app/src/main/java/braid/society/secret/lunarmochivox/listener/command/VcCommand.java
+++ app/src/main/java/braid/society/secret/lunarmochivox/listener/command/VcCommand.java
@@ -3,6 +3,7 @@ package braid.society.secret.lunarmochivox.listener.command;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionContextType;
+import net.dv8tion.jda.api.managers.AudioManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +19,21 @@ public class VcCommand extends CommandCascade {
   @Override
   protected void respond(SlashCommandInteractionEvent event) {
     final User author = event.getUser();
-    log.trace("Received VC command from user {}", event.getUser().getAsTag());
+    log.trace("Received VC command from user {}", author.getAsTag());
+    if (checkConnected(event)) {
+      fireDisconnect(event);
+    }
+    fireConnect(event);
+  }
+
+  private boolean checkConnected(SlashCommandInteractionEvent event) {
+    AudioManager audioManager = event.getMember().getGuild().getAudioManager();
+    return audioManager.isConnected();
+  }
+
+  private void fireDisconnect(SlashCommandInteractionEvent event) {
+  }
+
+  private void fireConnect(SlashCommandInteractionEvent event) {
   }
 }
