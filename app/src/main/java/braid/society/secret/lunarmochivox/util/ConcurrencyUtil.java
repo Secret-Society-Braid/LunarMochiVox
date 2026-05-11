923e4f1c808acd77d8e5a34d9bdf98fa4acefd20
iDOL_Ranfa_M@STER <hizumiaoba@gmail.com>
2026-05-11 16:50:16 +0900

    Replace CountingThreadFactory with custom NumberedThreadFactory for improved control over thread naming and daemon setting

--- app/src/main/java/braid/society/secret/lunarmochivox/util/ConcurrencyUtil.java
+++ app/src/main/java/braid/society/secret/lunarmochivox/util/ConcurrencyUtil.java
@@ -1,13 +1,37 @@
 package braid.society.secret.lunarmochivox.util;
 
 import java.util.concurrent.ThreadFactory;
-import net.dv8tion.jda.internal.utils.concurrent.CountingThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+import org.jspecify.annotations.NonNull;
 
 public class ConcurrencyUtil {
 
   private ConcurrencyUtil() {}
 
   public static ThreadFactory createThreadFactory(String threadName) {
-    return new CountingThreadFactory(() -> "LunarMochiVox", threadName);
+    return new NumberedThreadFactory(threadName);
+  }
+
+  private static class NumberedThreadFactory implements ThreadFactory {
+    private final Supplier<String> nameSupplier;
+    private final AtomicLong counter = new AtomicLong(0);
+    private final boolean daemon;
+
+    NumberedThreadFactory(String specifier, boolean daemon) {
+      this.nameSupplier = () -> "LunarMochiVox-%s-%s".formatted(specifier, counter.getAndIncrement());
+      this.daemon = daemon;
+    }
+
+    NumberedThreadFactory(String specifier) {
+      this(specifier, true);
+    }
+
+    @Override
+    public Thread newThread(@NonNull Runnable r) {
+      Thread thread = new Thread(r, nameSupplier.get());
+      thread.setDaemon(daemon);
+      return thread;
+    }
   }
 }
